6268c337207620e1b2356c161516555ff46c0125
Francisco <fnaveira11@gmail.com>
2021-03-16 17:04:16 -0400

    commit tree 25eac8d6b0d1441a8e38552bec856a5390b8e5df

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
 // Code your solution in this file!
+
+function distanceFromHqInBlocks(blocks){ 
+    if  (blocks >= 42){
+        return blocks - 42;}
+    else { 
+        return (blocks- 42)* -1;}
+}
+
+function distanceFromHqInFeet(blocks){
+    return distanceFromHqInBlocks(blocks) * 264;
+}
+
+
+
+
+function distanceTravelledInFeet(startBlock, endBlock){
+    if (endBlock - startBlock > 0){
+        return (endBlock - startBlock) * 264; }
+    else{ 
+        return (startBlock - endBlock) * 264; }         
+}
+
+function calculatesFarePrice(startBlock, endBlock){
+    if (distanceTravelledInFeet(startBlock, endBlock) > 2500){
+        return 'cannot travel that far';
+    }
+    else if (distanceTravelledInFeet(startBlock, endBlock) >= 2000){
+        return 25;
+    }
+    else if (distanceTravelledInFeet(startBlock, endBlock) > 400){
+        return (distanceTravelledInFeet(startBlock, endBlock) -400) * .02;
+    }
+    else {
+        return 0;
+    }
+}
